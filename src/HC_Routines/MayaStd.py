246514d8daa8e673efbf650ada978904bd16dd1b
vfxnake <jazzjoe@gmail.com>
2021-02-22 19:12:20 -0600

    adding maya python std

--- src/HC_Routines/MayaStd.py
+++ src/HC_Routines/MayaStd.py
@@ -1,10 +1,10 @@
-import maya.standalone
+import maya.standalone 
 
 import maya.cmds as cmds
 
 class MayaStd(object):
     def __init__(self):
-        maya.standalone.initialize(name='python')
+        maya.standalone.initialize( name='python' )
 
 if __name__ == "__main__":
     StartMaya = MayaStd()
