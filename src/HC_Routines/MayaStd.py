c41fa1e311fb4fa2ffb83e8c6063b7da8e923394
vfxnake <jazzjoe@gmail.com>
2021-02-22 19:25:00 -0600

    maya standalone initialize

--- src/HC_Routines/MayaStd.py
+++ src/HC_Routines/MayaStd.py
@@ -1,10 +1,12 @@
-import maya.standalone 
+import maya.standalone
 
 import maya.cmds as cmds
 
 class MayaStd(object):
     def __init__(self):
+        maya.standalone.initialize()
         maya.standalone.initialize( name='python' )
+        
 
 if __name__ == "__main__":
     StartMaya = MayaStd()
